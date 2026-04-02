532fc15d3e3bc80f438f647ecf7eb6f0173526e2
hungrychef-bytescode <hungrychefbytescode@gmail.com>
2026-04-02 12:40:42 +0800

    feat: initialize supabase and used riverpod for state management

--- lib/main.dart
+++ lib/main.dart
@@ -2,81 +2,23 @@
 
 //dependencies
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:agap/features/auth/pages/splashscreen.dart';
-import 'package:agap/theme/color.dart';
+import 'package:agap/core/services/supabase_service.dart';
+import 'package:agap/agap.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-//checks if supabase is configured
-bool _isSupabaseConfigured = false;
+/*
+initializes supabase and runs the app
+ProviderScope is used for Riverpod state management
+*/
 
 void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
 
-  await _initializeSupabaseIfConfigured();
+  await SupabaseService.initialize();
   
-  runApp(const MyApp());
-}
-
-// initialize supabase if env vars are present, otherwise skip to avoid runtime errors in dev/test environments without .env file
-Future<void> _initializeSupabaseIfConfigured() async {
-
-  //load env vars from .env file
-  try {
-    await dotenv.load(fileName: '.env');
-  } catch (e) {
-    debugPrint(' .env not found: $e');
-    return;
-  }
-
-  //read supabase config from env vars
-  final supabaseUrl = dotenv.env['SUPABASE_URL'];
-  final supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY'];
-
-  //debug log for verifying that env vars are loaded correctly
-  debugPrint('SUPABASE_URL: $supabaseUrl');
-  debugPrint('SUPABASE_ANON_KEY: ${supabaseAnonKey != null ? 'found' : 'missing'}');
-
-  //validation: anon key and url are non-empty
-  if (supabaseUrl == null || supabaseUrl.isEmpty ||
-      supabaseAnonKey == null || supabaseAnonKey.isEmpty) {
-    
-    debugPrint('Supabase env vars missing or empty, skipping init.');
-    return;
-  }
-
-  //initialize supabase with config from .env
-  await Supabase.initialize(
-    url: supabaseUrl,
-    anonKey: supabaseAnonKey,
+  runApp(const ProviderScope(
+    child: Agap()
+    )
   );
-
-  //set flag to true. supabase is successfully initialized
-  _isSupabaseConfigured = true;
-  debugPrint('Supabase initialized successfully.');
-}
-
-
-//root widget
-//configures global theme and initial route
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'AGAP',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: AppColors.agapOrange,
-          primary: AppColors.agapOrange,
-        ),
-        scaffoldBackgroundColor: AppColors.agapOrange,
-        useMaterial3: true,
-      ),
-      home: const SplashPage(),
-    );
-  }
 }
