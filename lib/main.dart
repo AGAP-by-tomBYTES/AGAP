2e0c8962d881fdc39543a31d14babdad0edfe686
hungrychef-bytescode <hungrychefbytescode@gmail.com>
2026-04-02 16:57:44 +0800

    feat: add Hive init in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@
 
 //dependencies
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:agap/core/services/supabase_service.dart';
 import 'package:agap/agap.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 
 /*
-initializes supabase and runs the app
+initialize supabase, hive, and run the app
 ProviderScope is used for Riverpod state management
 */
 
@@ -15,8 +17,11 @@ void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Hive.initFlutter();
+  await Hive.openBox("app_cache");
+
   await SupabaseService.initialize();
-  
+
   runApp(const ProviderScope(
     child: Agap()
     )
