b0a9b3a549f3c9464a20bc39e5474307865f9905
hungrychef-bytescode <hungrychefbytescode@gmail.com>
2026-04-03 10:38:40 +0800

    meta: pubspec dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+
 import 'package:agap/core/services/supabase_service.dart';
 import 'package:agap/agap.dart';
 
